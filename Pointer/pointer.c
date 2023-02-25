8290278fa34eb81216c0056a24e683e3b5e85c3c
jabed366 <jabedhasan231@gmail.com>
2023-02-25 15:41:41 +0600

    basic pointer

---
+++ Pointer/pointer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (){
+    int a = 10;
+    int *p = &a;
+
+    //asterisk sign point out the value of  memory address
+    printf("%d\n", *p);//10
+    printf("%d\n", *p+1);//11
+
+    //without asterisk sign it point out the value of memory address 
+    printf("%d\n",p);//6422296
+    
+    // as int data type takes 4 bytes and 
+    //that why increment 4 that indicates next memory for another variable
+    printf("%d\n",p+1);//6422300
+
+    return 0;
+} 
