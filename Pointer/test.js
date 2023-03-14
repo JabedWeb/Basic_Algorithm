a96b08f8ca0b394dc68617eea72d8ca70677a3d5
jabed366 <jabedhasan231@gmail.com>
2023-03-14 19:44:17 +0600

    added

--- Pointer/test.js
+++ Pointer/test.js
@@ -1,5 +1,2 @@
-
-Onessa Demonceau
-Graphiste
-March 4, 2015, Onessa managed Sébastien directlyMarch 4, 2015, Onessa managed Sébastien directly
-J'ai travaillé avec Sébastien - tant en formation, qu'en graphisme, ce fut une personne sur lequel je pouvais compter dessus pour m'épauler et être assidu dans son travail. Il savait gérer le stress et les challenges des clien
+//Which things will return you all the cookies in a string?
+// answer : window.document.cookie
