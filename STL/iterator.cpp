92ee9d596bf3d408c01f3cd46c08d7a8f68a0b0b
jabed366 <jabedhasan231@gmail.com>
2023-03-17 18:21:35 +0600

    iterator

--- STL/iterator.cpp
+++ STL/iterator.cpp
@@ -4,29 +4,16 @@ const int INF = 1e9;
 
 
 int main(){
-    // int array[3];
-    // array[0] = 1;
-    // array[1] = 2;
-
-    // cout<<array[0]<<endl;
-    // cout<<array[10]<<endl;
-    // cout<<array[10]<<endl;
-
+ 
     //vector
     vector <int> v;
-    v.push_back(1); // first element
-    v.push_back(2); // second element
-    v.push_back(10); // third element
+   for(int i = 0; i < 10; i++){
+       v.push_back(i);
+   }
 
-    for(int i=0;i<v.size();i++){
-        cout<<v[i]<<"\t";
+    // print vector using iterator
+    vector <int>::iterator it;
+    for(it = v.begin(); it != v.end(); it++){
+        cout<<*it<<"\t";
     }
-    puts("");
-
-    cout<<v[0]<<endl;
-
-    // another way to set vector
-    v[0] = 5;
-    cout<<v[0]<<endl;
-    cout<<v[10]<<endl;
-}
+}  
